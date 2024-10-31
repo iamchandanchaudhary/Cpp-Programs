7d77c0a3a1612df352f734f5ae7595bbc2fd641a
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-10-31 13:52:52 +0530

    Improvement 07

---
+++ Practice C++/Practice 4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "String Program:- \n" << endl;
+
+    string firstName = "Chandan";
+    string lastName = "Chaudhary";
+
+    string fullName = firstName + " " + lastName;
+
+    cout << "Full Name : " + fullName << endl;
+
+    return 0;
+}
