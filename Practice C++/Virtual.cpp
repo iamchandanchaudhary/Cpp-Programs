d2301578985109f05172bdf163db2882abe6e6e7
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-11-19 09:18:06 +0530

    Improvement 09

---
+++ Practice C++/Virtual.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Base {
+    public:
+
+    virtual void getValue() {
+        cout << "Base Class." << endl;
+    }
+};
+
+class Derived : public Base {
+    public:
+
+    void getValue() {
+        cout << "Derived Class." << endl;
+    }
+};
+
+int main() {
+    cout << "Virtual Function :- \n" << endl;
+
+    Base *ptr;
+    Derived aa;
+
+    ptr = &aa;
+
+    ptr->getValue();
+
+    return 0;
+}
