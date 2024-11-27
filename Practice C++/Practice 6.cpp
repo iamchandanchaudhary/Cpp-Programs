1776cd51f28630d59032552a5c052018aff7a855
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-11-27 09:50:01 +0530

    Improvement 12

--- Practice C++/Practice 6.cpp
+++ Practice C++/Practice 6.cpp
@@ -4,11 +4,17 @@ using namespace std;
 int main() {
     cout << "Table with while loop :- \n" << endl;
 
-    int n = 5;
+    int n;
+    cout << "Enter Number : ";
+    cin >> n;
+
     int i = 1;
 
+    cout << "The Table of " << n << " -" << endl;
     while(i <= 10) {
         cout << n << "*" << i << " = " << n * i << endl;
         i++;
     }
+
+    cout << "\nThank You :)" << endl;
 }
