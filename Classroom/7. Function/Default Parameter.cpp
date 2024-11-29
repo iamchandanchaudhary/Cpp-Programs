3f6677bc285a15adfc83b228ad38af9cb10042c4
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-11-29 09:22:50 +0530

    Improvement 14

--- Classroom/7. Function/Default Parameter.cpp
+++ Classroom/7. Function/Default Parameter.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void getValue(int a, int b = 60) {
+void getValue(int a, int b = 60) { // Only last argument can be Default
 
     int sum = a + b;
     cout << "Sum: " << sum << endl;
@@ -11,6 +11,7 @@ int main() {
     cout << "Default Parameter:- \n" << endl;
 
     getValue(45);
-    getValue(14, 25); // 1st argument can Not be Default
+    getValue(14, 25);
+
     return 0;
 }
