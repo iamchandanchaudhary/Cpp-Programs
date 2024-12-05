4394d6c3426f536637d92be295fe4d6ac36bf2a0
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-12-05 14:38:07 +0530

    Improvement 16

---
+++ Practice C++/Practice 8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Loop :- \n" << endl;
+
+    int n = 5;
+
+    for(int i = 0; i < n; i++) {
+        cout << i << ". Chandan Chaudhary" << endl;
+
+    }
+        return 0;
+}
